713a3e1592ed650620aad07fdc65679c83bec51e
henriqueFinco <127436903+henriqueFinco@users.noreply.github.com>
2024-08-29 14:39:52 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,53 @@
+package Classe7;
+
+public class Cliente{
+    //atributos do cliente/usuário
+    private String nome;
+    private String cpf;
+    private int idade;
+    private int telefone;
+    
+
+    //construtor
+    public Cliente(String nome, String cpf, int idade, int telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.telefone = telefone;
+    }
+
+    //getters e setters
+    //nome
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    //cpf
+    public String getCpf(){
+        return cpf;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    //idade
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    //telefone
+    public int getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(int telefone){
+        this.telefone = telefone;
+    }
+    
+    // Método para mostrar as informações da pessoa
+    public void exibirDados() {
+        System.out.println("Nome: "+nome+"CPF: "+cpf+"Idade: "+idade+"Telefone: "+telefone);
+    }
+}
