2a8df27e998b78c9fdb942e8e81138983ee5fda0
unknown <nicolas.s.fernandes@unesp.br>
2024-09-04 23:35:24 -0300

    test

--- Reserva.java
+++ Reserva.java
@@ -1,8 +1,12 @@
 import Hotel;
 
 import Cliente;
+import java.util.ArrayList;
+
+import com.mycompany.hotel.CheckIn;
 
 public class Reserva {
+    protected static ArrayList<Reserva> reservas = new ArrayList<Reserva>(); //verificar com o professor se isso vai dar certo
     protected CheckIn dataEntrada;
     protected CheckOut dataSaida;
     protected Quarto quarto;
@@ -17,10 +21,16 @@ public class Reserva {
             this.valorTotal = q.getValorDiaria();
             this.cliente = c;
             q.setDisponibilidade(false);
+            reservas.add(this); //Adiciona assim que a reserva é feita
         }else{
             throw new IllegalArgumentException("Quarto ocupado"); //justificativa: não é possível reservar um quarto ocupado
         }
-        
+            reservas.add(this);
+    }
+
+     
+    public static ArrayList<Reserva> getReservas() { // Método para obter todas as reservas (opcional)
+        return reservas;
     }
 
     public CheckIn getDataEntrada(){
