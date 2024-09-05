a884d9f130c898dd467a8d20bee9f7957e0c35a3
FernandoSalesi <fernando.k.salesi@unesp.br>
2024-09-05 05:32:20 -0300

    ServicoAdicional v2

--- ServicoAdicional.java
+++ ServicoAdicional.java
@@ -8,34 +8,65 @@ package Hotel;
  *
  * @author Fernando
  */
+import java.util.ArrayList;
+
 public class ServicoAdicional {
-    // atributos
-    private String descricao; //podendo ser spa, restaurante, limpeza diária
-    private float valor; //valor do serviço adicional em si
-    private Cliente cliente; //Cliente que solicitou o servico adicional
-    
-    // construtor
-    public ServicoAdicional(String descricao, float valor, Cliente c) {
-        this.descricao = descricao;
-        this.valor = valor;
-        this.cliente = c;
+
+    private ArrayList<Servico> servicos; //servicos gerenciados
+    private Cliente cliente;            //Cliente que solicitou o servico
+    private double valorTotal;          //valor total de todos os servicos
+    //private Quarto quarto;
+
+    public ServicoAdicional() {
     }
 
-    // getters e setters
-    public String getDescricao() {
-        return descricao;
+    public ServicoAdicional(Cliente cliente) {
+        this.cliente = cliente;
+        this.servicos = new ArrayList();
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    //  metodo que adiciona um servico
+    public void adicionarServico(Servico servico) {
+        this.servicos.add(servico);
     }
 
-    public float getValor() {
-        return valor;
+    // metodo para remover um servico especifico
+    public void removerServico(Servico servico) {
+        servicos.remove(servico);
     }
 
-    public void setValor(float valor) {
-        this.valor = valor;
+    // metodo que remove todos os servicos de um cliente
+    public void removerTodosServicos() {
+        servicos.clear();
+    }
+
+    // metodo para exibir todos os servicos adicionais solicitados (no console)
+    public void exibirServicos() {
+        System.out.println("\n Segue todos os servicos solicitados por "+this.cliente.getNome()+": \n");
+        for (int i = 0; i < servicos.size(); i++) {
+            Servico servico = servicos.get(i);
+            servico.exibir();
+        }
+    }
+
+    // metodo para buscar servico especifico
+    public Servico buscarPorNome(String nome) {
+        for (int i = 0; i < servicos.size(); i++) {
+            Servico servico = servicos.get(i);
+            if (servico.getNome().equals(nome)) {
+                return servico; // servico encontrado
+            }
+        }
+        return null; // servico nao encontrado
+    }
+
+    //  metodos getters e setters
+    public ArrayList<Servico> getServicos() {
+        return servicos;
+    }
+
+    public void setServicos(ArrayList<Servico> servicos) {
+        this.servicos = servicos;
     }
 
     public Cliente getCliente() {
@@ -44,5 +75,20 @@ public class ServicoAdicional {
 
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
-    } 
+    }
+    
+    //  getValorTotal vai retornar todos os precos dos servicos solicitados somados
+    public double getValorTotal() {
+        this.valorTotal = 0;
+        for (int i = 0; i < servicos.size(); i++) {
+            Servico servico = servicos.get(i);
+            this.valorTotal=+servico.getPreco();
+        }
+        return this.valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
 }
