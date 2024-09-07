d721bb2cbe55126bf9363bb2a896355603ccf37a
FernandoSalesi <fernando.k.salesi@unesp.br>
2024-09-07 02:54:36 -0300

    Update v3

--- Hotel/Servico.java
+++ Hotel/Servico.java
@@ -1,26 +1,47 @@
-package Hotel;
+package Classes;
 
 //  Classe Servico que é gerenciada por ServicoAdicional, definindo cada servico individualmente.
-
 import java.time.LocalDateTime;
 
-
 public class Servico {
+
     //atributos
     private String nome;        // qual servico sera feito como spa, restaurante, etc...
-    private float preco;       // preco do servico
+    private double valor;       // preco do servico
     private LocalDateTime data;     // data do servico
-    
+
     //construtores
     public Servico() {
     }
 
-    public Servico(String nome, float preco) {
+    public Servico(String nome) {
         this.nome = nome;
-        this.preco = preco;
         this.data = LocalDateTime.now();
+        switch (nome) {
+            case "lavanderia":
+                this.valor = 25;
+                break;
+            case "spa":
+                this.valor = 100;
+                break;
+            case "cafe da manha":
+                this.valor = 20;
+                break;
+            case "cafe da tarde":
+                this.valor = 15;
+                break;
+            case "almoco":
+                this.valor = 30;
+                break;
+            case "janta":
+                this.valor = 30;
+                break;
+            case "limpeza":
+                this.valor = 60;
+                break;
+        }
     }
-    
+
     //metodos getters e setters
     public String getNome() {
         return nome;
@@ -30,17 +51,29 @@ public class Servico {
         this.nome = nome;
     }
 
-    public float getPreco() {
-        return preco;
+    public double getValor() {
+        return valor;
     }
 
-    public void setPreco(float preco) {
-        this.preco = preco;
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    //retorna somente o dia do servico como int
+    public int getDia() {
+        return this.data.getDayOfMonth();
     }
 
     //metodo que exibe os dados do servico no console
     public void exibir() {
-        System.out.println("\n Nome: "+nome+" Preco: "+preco+" Data: "+data);
+        System.out.println("\n Nome: " + nome + " Valor: " + valor + " Data: " + data.getDayOfMonth() + "/" + data.getMonthValue() + "/" + data.getYear());
     }
 }
-
