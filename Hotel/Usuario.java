6048e628177ef85b7eb19b5cc9c92eac0a13a7af
sleeper <nicolas.s.fernades@unesp.br>
2024-11-27 01:56:09 -0300

    Implementação de login/proxy/cadastro

--- Hotel/Usuario.java
+++ Hotel/Usuario.java
@@ -3,17 +3,16 @@ package Hotel;
 public class Usuario {
     protected static int nextID = 1;
     protected String nome;
-    protected String email;
+    protected String email; //copia para o cadastro
+    // protected String senha;
+    // protected String confirmacao; //Segunda vez colocando a senha pra confirmar
     protected String CPF;
     protected int ID; //ID n segnifica o numero de usuarios cadastrados, mas sim o numero de usuarios criados, pode haver eliminacao de usuarios
 
 
-    public Usuario(String nome, String email, String CPF) {
+    public Usuario(String nome, String email, String CPF){
         this.nome = nome;
         this.email = email;
-        // if(CPF.length() != 11){
-        //     throw new IllegalArgumentException("CPF inválido"); //Bloqueia a criação do objeto por invalidez de CPF
-        // }
         this.CPF = CPF;
         this.ID = nextID++;
     }
@@ -38,6 +37,8 @@ public class Usuario {
         return ID;
     }
 
+    //Somente para arrumar dados errados
+
     public void setCpf(String CPF){
         if(CPF.length() != 11){
             throw new IllegalArgumentException("CPF inválido"); //Bloqueia a modificação, caso seja necessario, por invalidez de CPF
@@ -58,3 +59,4 @@ public class Usuario {
     }
 
 }
+
